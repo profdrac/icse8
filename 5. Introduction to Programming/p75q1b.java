022c44a09da97f8606ee4606aa3a443194a17b23
profdrac <ashu4hire@gmail.com>
2021-08-23 12:04:36 +0530

    Add files via upload

---
+++ 5. Introduction to Programming/p75q1b.java
@@ -0,0 +1,7 @@
+class p75q1b
+{
+    public static void main()
+    {
+        System.out.println("Ashutosh, you are in class 8");
+    }
+}
---
+++ 5. Introduction to Programming/p75q1c.java
@@ -0,0 +1,8 @@
+class p75q1c
+{
+    public static void main()
+    {
+        System.out.println("Your school name is OLS");
+        System.out.println("The address is Dhalpur, Kullu - 175101");
+    }
+}
