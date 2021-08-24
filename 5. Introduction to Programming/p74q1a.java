2b31e57ae4d71eae48fce4a529dc3d07787802e2
profdrac <ashu4hire@gmail.com>
2021-08-24 11:59:48 +0530

    Add files via upload

---
+++ 5. Introduction to Programming/p74q1a.java
@@ -0,0 +1,12 @@
+//WAP to show area and perimeter of a square of side 12cms
+class p74q1a
+{
+    public static void main()
+    {
+        int side = 12;
+        int area = side*side;
+        int peri = 4*side;
+        System.out.println(area);
+        System.out.println(peri);
+    }
+}
---
+++ 5. Introduction to Programming/p75q1d.java
@@ -0,0 +1,13 @@
+//WAP to show area and circumference of a circle
+
+class p75q1d
+{
+    public static void main()
+    {
+        int r = 7;
+        double area = 3.14*r*r;
+        double circum = 2*3.14*r;
+        System.out.println(area);
+        System.out.println(circum);
+    }
+}
