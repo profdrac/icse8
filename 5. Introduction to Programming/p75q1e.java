53f86bb32243d5e3ec7d8d3535dacf13e200104d
profdrac <ashu4hire@gmail.com>
2021-08-31 12:01:46 +0530

    Add files via upload

---
+++ 5. Introduction to Programming/p75q1e.java
@@ -0,0 +1,9 @@
+//WAP to show height and weight on screen
+class p75q1e
+{
+    public static void main()
+    {
+        System.out.println("Your height is 5 ft");
+        System.out.println("Your weight is 45 kg");
+    }
+}
