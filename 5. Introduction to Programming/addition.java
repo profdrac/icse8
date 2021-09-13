dbff9ed6b721f68d241f652b68a9ef05eafe073f
profdrac <ashu4hire@gmail.com>
2021-09-13 12:02:55 +0530

    Add files via upload

---
+++ 5. Introduction to Programming/addition.java
@@ -0,0 +1,11 @@
+class addition
+{
+    public static void main()
+    {
+        int a,b;
+        a=10;
+        b=20;
+        int result = a+b;
+        System.out.println("Sum of "+a+" and "+b+" is "+result);
+    }
+}
---
+++ 5. Introduction to Programming/better_info.java
@@ -0,0 +1,14 @@
+class better_info
+{
+    public static void main()
+    {
+        String myname;
+        myname = "Ashutosh Sharma";
+        int rollno;
+        rollno = 20;
+        String school = "OLS SCHOOL";
+        System.out.println("My name is "+myname);
+        System.out.println("My roll number is "+rollno);
+        System.out.println(school+ " is my school");
+    }
+}
