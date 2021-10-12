a3e10dce96d0c6c97dfe854dbf38d6f374abad17
profdrac <ashu4hire@gmail.com>
2021-10-12 12:01:21 +0530

    Add files via upload

---
+++ 6. Conditional Statements in Java/evenodd.java
@@ -0,0 +1,16 @@
+//WAP to print whether given no is even or odd
+
+class evenodd
+{
+    public static void main(int n)
+    {
+        if(n%2==0)
+        {
+            System.out.println("Number is even");
+        }
+        else
+        {
+            System.out.println("Number is odd");
+        }
+    }
+}
---
+++ 6. Conditional Statements in Java/voting.java
@@ -0,0 +1,14 @@
+class voting
+{
+    public static void main(int age)
+    {
+        if(age>=18)
+        {
+            System.out.println("Eligible to vote");
+        }
+        else
+        {
+            System.out.println("You cannot vote yet");
+        }
+    }
+}
