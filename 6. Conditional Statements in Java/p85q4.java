7c9397c6c26388cfc719e1251b6dba435b336723
profdrac <ashu4hire@gmail.com>
2021-11-09 10:49:49 +0530

    Add files via upload

---
+++ 6. Conditional Statements in Java/p85q4.java
@@ -0,0 +1,15 @@
+//WAP to get a number from user and print whether its divisible by 5 or not
+class p85q4
+{
+    public static void main(int n)
+    {
+        if(n%5 == 0)
+        {
+            System.out.println(n+" is divisible by 5");
+        }
+        else
+        {
+            System.out.println(n+" is not divisible by 5");
+        }
+    }
+}
---
+++ 6. Conditional Statements in Java/p85q5.java
@@ -0,0 +1,16 @@
+//WAP to get percentage marks of a student and show whether the student gets a scholarship or not
+//if per>=75 student is eligible for scholarship otherwise not
+class p85q5
+{
+    public static void main(int per)
+    {
+        if(per>=75)
+        {
+            System.out.println("Eligible for scholarship");
+        }
+        else
+        {
+            System.out.println("Better luck next time!");
+        }
+    }
+}
